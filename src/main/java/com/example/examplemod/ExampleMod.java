8c5b590e2b16aba4565780d862cf552b26df0fc1
AterAnimAvis <AterAnimAvis@gmail.com>
2020-12-04 17:02:22 +0000

    General Cleanup
    Signed-off-by: AterAnimAvis <AterAnimAvis@gmail.com>

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,11 +1,14 @@
 package com.example.examplemod;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -13,15 +16,20 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
-{
+@Mod(ExampleMod.MOD_ID)
+public class ExampleMod {
+
+    // The value here should match an entry in the META-INF/mods.toml file
+    public static final String MOD_ID = "example_mod";
+    public static final String MOD_NAME = "Example Mod";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
---
+++ src/main/java/com/example/examplemod/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package com.example.examplemod;
+
+import javax.annotation.ParametersAreNonnullByDefault;
